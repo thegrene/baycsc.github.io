46a6ea6ada6ecd005a61035432740c1d60c3c5f2
p-Lennart <112364552+p-Lennart@users.noreply.github.com>
2022-09-21 20:55:08 -0700

    Revamp Back-end of Events Page
    - Moved all event data into object entries in a js file
    - Cleared all events in Events.html; made a template node to render new ones
    - Created a script to render event data dynamically in Events.html

---
+++ assets/js/eventRenderer.js
@@ -0,0 +1,55 @@
+// var tester = () => {
+// 	const output = [];
+// 	for (const d of document.querySelectorAll('.col-12-medium')) {
+// 		var contents = d.querySelector('section section');
+        
+// 		const data = {
+            // imageURL: contents.querySelector('a.image img').src,
+            // title: contents.querySelector('h3').innerText,
+            // description: contents.querySelector('p').innerText,
+// 		}
+
+// 		if (!contents.querySelector('a.image').href.endsWith('#')) {
+// 			data.learnMoreURL = contents.querySelector('a.image').href;
+// 		} else if (contents.querySelector('ul.actions a.button')) {
+            // data.learnMoreURL = contents.querySelector('ul.actions a.button').href;
+// 		} else {
+// 			data.learnMoreURL = null;
+// 		}
+
+// 		output.push(data);
+// 	}
+
+// 	console.log(output);
+// }
+
+window.onload = () => {
+    var counter = [0, 0];
+
+    let targets = [
+        document.querySelector('div#eventsFuture'),
+        document.querySelector('div#eventsPast'),
+    ]
+
+    for (const e of eventData) {
+        
+        let template = document.querySelector('span#eventTemplate div');
+        const clone = template.cloneNode(true);
+        
+        clone.querySelector('a.image img').src = e.imageURL;
+        clone.querySelector('h3').innerText = e.title;
+        clone.querySelector('p').innerText = e.description;
+        clone.querySelector('ul.actions a.button').href = e.learnMoreURL;
+
+        let category = e.future ? 0 : 1;
+        
+        counter[category] += 1;
+        
+        if (counter[category] > 3) {
+            clone.querySelector("section.topBorder").appendChild(document.createElement("hr"));
+        } 
+
+        targets[category].appendChild(clone);
+
+    }
+}
